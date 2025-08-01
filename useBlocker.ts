ddd189dd648bb530fe106eda5cdff779cff50231
Pallavi Amit Kumar <pallavizanje@gmail.com>
2025-08-01 09:26:33 +0530

    Update and rename use prompt.ts to useBlocker.ts

---
+++ useBlocker.ts
@@ -0,0 +1,24 @@
+// hooks/useBlocker.ts
+import { useContext, useEffect } from 'react';
+import { UNSAFE_NavigationContext as NavigationContext } from 'react-router-dom';
+
+export function useBlocker(blocker: (tx: any) => void, when = true) {
+  const { navigator } = useContext(NavigationContext);
+
+  useEffect(() => {
+    if (!when) return;
+
+    const unblock = navigator.block((tx: any) => {
+      const autoUnblockingTx = {
+        ...tx,
+        retry() {
+          unblock();
+          tx.retry();
+        },
+      };
+      blocker(autoUnblockingTx);
+    });
+
+    return unblock;
+  }, [navigator, blocker, when]);
+}
