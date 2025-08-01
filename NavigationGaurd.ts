88c663c2b4e19ab88c38188d1b1cc3ca35f09186
Pallavi Amit Kumar <pallavizanje@gmail.com>
2025-08-01 10:11:22 +0530

    Update and rename MyFormPage.tsx to NavigationGaurd.ts

---
+++ NavigationGaurd.ts
@@ -0,0 +1,26 @@
+import { useContext, useEffect } from "react";
+import {
+  UNSAFE_NavigationContext as NavigationContext,
+  useLocation
+} from "react-router-dom";
+
+export function useBlocker(blocker: (tx: any) => void, when = true) {
+  const navigator = useContext(NavigationContext).navigator;
+
+  useEffect(() => {
+    if (!when) return;
+
+    const unblock = navigator.block((tx: any) => {
+      const autoUnblockingTx = {
+        ...tx,
+        retry() {
+          unblock();
+          tx.retry();
+        }
+      };
+      blocker(autoUnblockingTx);
+    });
+
+    return unblock;
+  }, [navigator, blocker, when]);
+}
